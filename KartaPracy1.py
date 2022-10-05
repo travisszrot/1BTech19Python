87e20c1ebd4953ede1e7d8e8324d4510d3b33937
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-05 12:12:46 +0000

    zrobione zadania

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,7 +1,31 @@
-#Zad 1
+# Zad 1
 
 a = int(input())
 b = int(input())
-print(a**2 + b**2)
+print(a*a + b*b)
 
-#Zad 2
+# Zad 2
+
+a = int(input())
+b = int(input())
+print((a+b)**2)
+
+# Zad 3
+
+a = int(input())
+b = int(input())
+print((a-b)**3)
+
+# Zad 4
+
+#a, b, c =map(int,input().splitt())
+#print(a+b+c)
+
+# Zad 6 
+
+brutto =float(input))
+print(brutto / 1.23)
+
+# Zad 7 
+a,b = int(input()), int(input())
+print(a % b)

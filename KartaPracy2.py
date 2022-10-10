3c89edc4a7045fafc02db142f9d0af39ceb4054e
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-10 10:20:07 +0000

    dodano plik, zadania 1 2 3

---
+++ KartaPracy2.py
@@ -0,0 +1,20 @@
+#Zad 1
+n = int(input())
+if n % 3 ==0:
+  print("TAK")
+else:
+  print("NIE")
+
+#Zad 2
+n = int(input())
+if n >= 100 and n < 1000 and n % 17 == 0:
+  print("TAK")
+else:
+  print("NIE") 
+#Zad 3
+
+wiek = int(input())
+if wiek >= 18:
+  print("TAK jesteś pełnoletni")
+else:
+  print("NIE jesteś pełnoletni")

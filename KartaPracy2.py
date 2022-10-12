33d1fe1edb856ce67e5fd0e638ba2c75e82dab02
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-12 12:15:03 +0000

    nowe rozwiązania

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,20 +1,46 @@
-#Zad 1
-n = int(input())
-if n % 3 ==0:
-  print("TAK")
-else:
-  print("NIE")
+# # # #Zad 1
+# # # n = int(input())
+# # # if n % 3 ==0:
+# # #   print("TAK")
+# # # else:
+# # #   print("NIE")
 
-#Zad 2
-n = int(input())
-if n >= 100 and n < 1000 and n % 17 == 0:
-  print("TAK")
-else:
-  print("NIE") 
-#Zad 3
+# # # #Zad 2
+# # # n = int(input())
+# # # if n >= 100 and n < 1000 and n % 17 == 0:
+# # #   print("TAK")
+# # # else:
+# # #   print("NIE") 
+# # # #Zad 3
 
-wiek = int(input())
-if wiek >= 18:
-  print("TAK jesteś pełnoletni")
-else:
-  print("NIE jesteś pełnoletni")
+# # # wiek = int(input())
+# # # if wiek >= 18:
+# # #   print("TAK jesteś pełnoletni")
+# # # else:
+# # #   print("NIE jesteś pełnoletni")
+
+# # # #Zad 5
+
+# # # a = int(input())
+# # # b = int(input())
+# # # c = int(input())
+
+# # if (c>a and c<b) or (c>b and c<a):
+# #    print("TAK")
+# # # else:
+# # #   print("NIE")
+
+# #Zad 6
+
+# a = int(input())
+# p = int(input())
+# if (a**p-a) % p== 0:
+#   print("TAK, spełnia MTF")
+# else: 
+#   print("NIE, nie spełnia MTF")
+
+#Zad 7
+
+k = int(input())
+
+

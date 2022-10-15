22295ac8e70d780ec01d2ddfed895a027dcfe8ff
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-15 15:20:19 +0000

    wykonano zadania 1,2

---
+++ KartaPracy2a.py
@@ -0,0 +1,16 @@
+# #ZAD 1
+
+# a,b = int(input()),int(input())
+# if (a + b) % 2 ==0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# # ZAD 2
+
+a = int(input())
+g = int(input())
+if (a+g%2)==(a*g):
+  print("TAK")
+else:
+  print("NIE")

3d8669b8cb673151e504e97bc9ba2da9b4760809
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-16 12:56:01 +0000

    wykonano wszystkie zadania z karty pracy 2a

--- KartaPracy2a.py
+++ KartaPracy2a.py
@@ -1,16 +1,63 @@
-# #ZAD 1
+# # # # #ZAD 1
 
-# a,b = int(input()),int(input())
-# if (a + b) % 2 ==0:
-#   print("TAK")
+# # # # a,b = int(input()),int(input())
+# # # # if (a + b) % 2 ==0:
+# # # #   print("TAK")
+# # # # else:
+# # # #   print("NIE")
+
+# # # # # # ZAD 2
+
+# # # # a = int(input())
+# # # # g = int(input())
+# # # # if (a+g%2)==(a*g):
+# # # #   print("TAK")
+# # # # else:
+# # # #   print("NIE")
+
+# # # # ZAD 3
+# # # k, l, m = int(input()), (input()), (input())
+# # # if k == l:
+# # #   print("NIE")
+# # # elif l == m:
+# # #   print("NIE")
+# # # elif m == k:
+# # #   print("NIE")
+# # # else:
+# # #   print("TAK")
+
+# # #ZAD 4
+
+# # a,b,c,d = int(input()),int(input()),int(input()),int(input())
+# # if a<b and a<c and a<d:
+# #   print("najniejszą liczbą jest (a)", a) 
+# # if b<a and b<c and b<d:
+# #   print("najniejszą liczbą jest (b)", b) 
+# # if c<a and c<b and c<a:
+# #   print("najniejszą liczbą jest (c)", c) 
+# # if d<a and d<b and d<c:
+# #   print("najniejszą liczbą jest (d)", d) 
+
+# # #ZAD 5
+
+# a,b,c = int(input()),int(input()),int(input())
+
+# if a+b>c and b+c>a and a+c>b:
+#   print ("liczby spełniają nierówność trójkąta")
 # else:
-#   print("NIE")
+#   print ("liczby nie spełniają nierównośći trójkąta")
 
-# # ZAD 2
+# #ZAD 6
 
-a = int(input())
-g = int(input())
-if (a+g%2)==(a*g):
-  print("TAK")
-else:
-  print("NIE")
+a,b,c = int(input()),int(input()),int(input())
+if a**2 < b**2 + c**2:
+  print("ostrokątny")
+elif a**2 + b**2 == c**2:
+  print("prostokątny")
+elif a**2 > b**2 + c**2:
+  print("rozwartokątny")
+
+
+  
+
+

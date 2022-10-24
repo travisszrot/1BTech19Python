5c6069b01c1e454d6b89ca0f6f0bac1df1efc954
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-24 10:20:56 +0000

    karta pracy 3 nowe rozwiązania

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,7 +1,32 @@
-#ZAD 1
+# # # #ZAD 1
+
+# # # n=int(input())
+
+# # # for i in range(n):
+# # #   print(i**3+3,end="")
+
+# # #ZAD 3
+
+# # n = int(input())
+# # for i in range(1,n+1):
+# #   if n% i == 0:
+# #     print(i,end=" ")
+    
+# #ZAD 4
+
+# s=0
+# for i in range(10,100):
+#   s = s + i 
+# print(s)
+
+
+#ZAD 5
 
 n=int(input())
+for i in range(n-1):
+  k=in = int(input())
+  
 
-for i in range(n):
-  print(i**3+3,end="")
+
+
 

3ad3ee348ff4e3de177a2e48378f5af6f3fe4644
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-10-31 18:03:37 +0000

    KartaPracy3b wykonano 3 zadania

---
+++ KartaPracy3b.py
@@ -0,0 +1,21 @@
+# #ZAD 1
+
+# print(list(range(1 , 32 ,1)))
+
+#ZAD 2
+
+
+# for y in range(1,100,2):
+#   print (y**2)
+
+
+
+#ZAD 3
+
+# for y in range(1137,9999,379):
+#   print(y)
+
+#ZAD 4
+
+for y in range(100,999):
+  print(

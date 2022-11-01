9d507de971ac920eceb313408193f0aefbe6c1cb
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-11-01 21:09:47 +0000

    wykonano wszystkie zadania

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -17,5 +17,60 @@
 
 #ZAD 4
 
-for y in range(100,999):
-  print(
+# for y in range(100,999):
+#   if y % 5==0 or  y % 6==0:
+#     print(y)
+
+
+# #ZAD 5 
+
+
+# x = int(input("ile liczb?"))
+# suma=0
+# for  i in range(x):
+#   k = int(input())
+#   suma+=k
+# print(suma)
+
+#ZAD 6
+
+# k = int(input())
+# groot=0
+# for i in range(2,k+1,2):
+#  groot+=i
+# print(a)
+
+# #zad 7
+# m = int(input())
+# a=0
+# for i in range(11, m+1, 2):
+#   a+=i
+# print(a)
+
+#Zad 8
+# W = int(input())
+# L = int(input())
+# c = 0
+# suma = W
+# for i in range(0, L * 12):
+#   c = suma * 0.06 * (1/12)
+#   suma += c
+# print(round(suma,2))
+
+
+#zad 9
+# n = int(input())
+# suma=0
+# for i in range(21,n+1,100):
+#  suma+=i
+# print(suma)
+
+
+#Zad 10
+# import math
+# for i in range(1, 1000):
+#  if i % 10 == math.sqrt(i):
+#    print(i)
+#  elif i % 100 == math.sqrt(i):
+#    print(i)
+

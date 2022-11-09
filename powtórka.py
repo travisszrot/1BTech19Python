83300819e5004c0c5824256d95e7b73941b7363b
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-11-09 13:15:25 +0000

    PrzedSprawdzianem

---
+++ powtórka.py
@@ -0,0 +1,48 @@
+# # #1
+# # # print(11%4.)
+
+
+# # # k = 25
+
+# # # print(k%5)
+
+# # # from math import sqrt
+# # # print(sqrt(49))
+# # # print(49**(1/2))
+
+# # # print(8**(1/3))
+
+
+# # #if
+
+# # == >= <= < > !=
+
+# liczba 3-cyfrowa
+# podzielnymi przez 
+# 13 i niepodzielnymi przez 4
+# for i in range (100,1000):
+#   if i % 13 == 0 and i % 4 != 0:
+#     print(i,end=" ")
+
+#łączenie warunków
+
+# a,b,c = 3 , 5 ,7
+# if a < b < c:
+#   print("Eppure si move")
+
+# n = 24
+
+# for i in range (1,25):
+#   if  n % i ==0:
+#     print(i)
+#     suma = suma + i
+# print("suma", suma)
+# print("ilość", ilosc)
+
+# k = 3
+# suma = 0 
+# ilosc = 0 
+# for i in range(100,100+k):
+#   suma = suma + i
+#   print(suma)
+

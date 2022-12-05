f04bb29d5e28cb13117a817fb94448c5f546b76c
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-12-05 11:19:58 +0000

    cezar

---
+++ Cezar.py
@@ -0,0 +1,32 @@
+# # funkcja ord - zwraca kod ASCII znaku
+# print(ord("A"))
+# print(ord("Z"))
+# # w python kod ASCII wielkich liter A-Z są od 65 do 90
+
+# # funkcja chr() - zwraca znak dla dawnego kodu
+# print(chr(66)
+# print(chr(75))
+
+# zadanie testowe 
+
+# for i in range(65,91):
+#   print(chr(i), end=" ")
+
+
+
+napis = "KOT"
+szyf = ""
+print(napis[0], napis[1], napis[2])
+
+for i in range(len(napis)):
+  print(napis[i])
+
+
+
+# print(napis[0], napis[1], napis[2])
+for i in range(len(napis)):
+  print(napis[i])
+  szyfr = szyfr + chr(ord(napis[i])+3)
+  print(szyfr)
+  
+

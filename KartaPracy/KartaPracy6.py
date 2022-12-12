5370539162f6416f3c164ed0d4370121d810912d
1B03FB <114583421+travisszrot@users.noreply.github.com>
2022-12-12 11:19:34 +0000

    zad 1-4 KartaPracy6

---
+++ KartaPracy/KartaPracy6.py
@@ -0,0 +1,26 @@
+# #zad 1
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+
+# if b - a == c - b: print("Jest artmetyczny")
+
+# if b / a == c / b: print("Jest geometryczny")
+
+# if a < b < c: print("Jest rosnący")
+
+
+# #zad 2
+
+# for i in range(100,1000):
+# suma=0
+
+# if i//8==0 and i //16!= 0:
+#   suma +=i
+#   print(i)
+
+#zad 4              
+
+for i in range(10,99):
+  print()
